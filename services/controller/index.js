a41e94ad3c3241d1bfc70b4cf3f866befd4f0c88
GAMASTI\ana.gaspar <ana.gaspar@moovefy.com.br>
2024-04-08 09:12:02 -0300

    Tarefa prática #1 - Microservices

--- services/controller/index.js
+++ services/controller/index.js
@@ -42,9 +42,18 @@ app.get('/shipping/:cep', (req, res, next) => {
     );
 });
 
-/**
- * Inicia o router
- */
+app.get('/product/:id', (req, res, next) => {
+    inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'something failed :(' });
+        } else {
+            res.json(product);
+        }
+    });
+});
+
+
 app.listen(3000, () => {
     console.log('Controller Service running on http://127.0.0.1:3000');
 });
